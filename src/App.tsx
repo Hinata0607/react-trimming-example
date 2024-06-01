f9cacebe2bfd13d8df926e040399d052adb8545b
Hinata0607 <atahin0607@gmail.com>
2024-06-02 03:56:47 +0900

    add: provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Box } from '@mui/material';
+import { UploadImages } from './components';
+
 function App() {
-	return <>aiueo</>;
+	return (
+		<>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column',
+					gap: '100px',
+					width: '1000px',
+					maxWidth: '100vw',
+					padding: '50px 0',
+					margin: '0 auto',
+				}}
+			>
+				<UploadImages />
+			</Box>
+		</>
+	);
 }
 
 export default App;
